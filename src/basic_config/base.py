180805628e0e7ba4629897c9ac463955d43f9de5
caibangyu <cbangyu@gmail.com>
2018-12-06 21:18:48 +0800

    add base and ForbiddenUselessService

---
+++ src/basic_config/base.py
@@ -0,0 +1,20 @@
+import logging
+import subprocess
+
+
+class Base(object):
+
+    def __init__(self, system, version):
+        self._logger= logging.getLogger(__name__)
+        self._system = system
+        self._version = version
+
+    def check(self):
+        pass
+
+    def set(self):
+        pass
+
+    def _run_command(self, command):
+        process = subprocess.Popen(command, stdout=subprocess.PIPE, shell=True)
+        return process.communicate()
---
+++ src/basic_config/forbidden_useless_service.py
@@ -0,0 +1,49 @@
+from . import base
+
+
+class ForbiddenUselessService(base.Base):
+    _devices = ["bluetooth", "rlogin", "rwho", "sh", "rexec"]
+
+    def __init__(self, system, version):
+        super(ForbiddenUselessService, self).__init__(system, version)
+        self._status = self.check_all()
+
+    def check_all(self):
+        ready_count = 0
+        for device in self._devices:
+            ready_count += self._check(device)
+        if ready_count < len(self._devices):
+            return False
+        else:
+            return True
+
+    def set_all(self, status):
+        if status == self._status:
+            return True
+        ready_count = 0
+        for device in self._devices:
+            ready_count += self._set(device, status)
+        if ready_count < len(self._devices):
+            return False
+        else:
+            return True
+
+    def _check(self, device):
+        cmd = "chkconfig --list {device}".format(device=device)
+        stdout, err = self._run_command(cmd.split())
+        if stdout.find(b"3:off") > 0 and stdout.find(b"5:off") > 0:
+            return 1
+        else:
+            return 0
+
+    def _set(self, device, status):
+        cmd = "chkconfig --level 35 {device} {status}"
+        set_status = "off"
+        if not status:
+            set_status = "on"
+        cmd = cmd.format(device=device, status=set_status)
+        stdout, err = self._run_command(cmd.split())
+        if not err:
+            return 1
+        else:
+            return 0
