c5685888ae91f99770a75fe82d9cc3ff59271877
caibangyu <cbangyu@gmail.com>
2018-12-07 19:27:40 +0800

    add ForbiddenMobileDevice

---
+++ src/basic_config/forbidden_mobile_device.py
@@ -0,0 +1,37 @@
+from . import base
+
+
+class ForbiddenMobileDevice(base.Base):
+
+    def __init__(self, system, version):
+        super(ForbiddenMobileDevice, self).__init__(system, version)
+        self._op_file = "/etc/security/console.perms"
+        self._first_status = self.check()
+        self._status = self._first_status
+
+    def check(self):
+        cmd = "grep '^#<console' '{op_file}'".format(op_file=self._op_file)
+        stdout, err = self._run_command(cmd)
+        if stdout.find(b"#<console") >= 0:
+            self._status = True
+        else:
+            self._status = False
+        return self._status
+
+    def set(self, status):
+        if status == self.check():
+            return
+        if status:
+            self._set()
+        else:
+            self._unset()
+
+    def _set(self):
+        cmd = "sed -i 's/^<console/^#<console/g' '{op_file}'".format(op_file=self._op_file)
+        self._run_command(cmd)
+        return self.check()
+
+    def _unset(self):
+        cmd = "sed -i 's/^#<console/^<console/g' '{op_file}'".format(op_file=self._op_file)
+        self._run_command(cmd)
+        return not self.check()
