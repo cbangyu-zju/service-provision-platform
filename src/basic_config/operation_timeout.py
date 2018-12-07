6deeb1f000f70a846a0011d1534f193cc48f8ccd
caibangyu <cbangyu@gmail.com>
2018-12-07 18:21:55 +0800

    fix PasswordPeriod

--- src/basic_config/operation_timeout.py
+++ src/basic_config/operation_timeout.py
@@ -1,10 +1,10 @@
 from . import base
 
 
-class PasswordPeriod(base.Base):
+class OperationTime(base.Base):
 
     def __init__(self, system, version):
-        super(PasswordPeriod, self).__init__(system, version)
+        super(OperationTime, self).__init__(system, version)
         self._op_file = "/etc/pam.d/system-auth"
         self._status = self.check()
 
