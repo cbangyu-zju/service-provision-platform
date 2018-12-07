c0595f4467bae9820952e493a3c1a366f73e5627
caibangyu <cbangyu@gmail.com>
2018-12-07 18:01:15 +0800

    finish l3

---
+++ src/basic_config/operation_timeout.py
@@ -0,0 +1,39 @@
+from . import base
+
+
+class PasswordPeriod(base.Base):
+
+    def __init__(self, system, version):
+        super(PasswordPeriod, self).__init__(system, version)
+        self._op_file = "/etc/pam.d/system-auth"
+        self._status = self.check()
+
+    def check(self):
+        cmd = "grep '^auth[[:space:]]required[[:space:]]pam_tally2.so[[:space:]]deny=5[[:space:]]unlock_time=600' " \
+              " '{op_file}' "
+        cmd = cmd.format(op_file=self._op_file)
+        stdout, err = self._run_command(cmd)
+        if stdout.find(b"auth") >= 0:
+            self._status = True
+        else:
+            self._status = False
+        return self._status
+
+    def set(self, status):
+        if status == self._status:
+            return
+        return self._set()
+
+    def _set(self):
+        if not self.check():
+            cmd = "echo 'auth required pam_tally2.so deny=5 unlock_time=600' >> '{op_file}'"
+            cmd = cmd.format(op_file=self._op_file)
+            self._run_command(cmd)
+        return True
+
+    def _unset(self):
+        if self.check():
+            cmd = "sed -i '/^auth.*required.*pam_tally2.so.*deny=5.*unlock_time=600/d' '{op_file}'".format(
+                op_file=self._op_file)
+            self._run_command(cmd)
+        return True
