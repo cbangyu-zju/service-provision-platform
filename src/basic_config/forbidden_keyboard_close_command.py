4a55592221e14de03bc4c296d333d6622e6e022b
caibangyu <cbangyu@gmail.com>
2018-12-07 19:22:30 +0800

    ForbiddenKeyboardCloseCommand

---
+++ src/basic_config/forbidden_keyboard_close_command.py
@@ -0,0 +1,37 @@
+from . import base
+
+
+class ForbiddenKeyboardCloseCommand(base.Base):
+
+    def __init__(self, system, version):
+        super(ForbiddenKeyboardCloseCommand, self).__init__(system, version)
+        self._op_file = "/etc/init/control-alt-delete.conf"
+        self._first_status = self.check()
+        self._status = self._first_status
+
+    def check(self):
+        cmd = "ls {op_file}".format(op_file=self._op_file)
+        stdout, err = self._run_command(cmd)
+        if stdout.find(bytes(self._op_file, encoding='utf8')) >= 0:
+            self._status = False
+        else:
+            self._status = True
+        return self._status
+
+    def set(self, status):
+        if status == self.check():
+            return
+        if status:
+            self._set()
+        else:
+            self._unset()
+
+    def _set(self):
+        cmd = "mv {origin} {end}".format(origin=self._op_file, end=self._op_file+".bak")
+        self._run_command(cmd)
+        return self.check()
+
+    def _unset(self):
+        cmd = "mv {origin} {end}".format(origin=self._op_file+".bak", end=self._op_file)
+        self._run_command(cmd)
+        return not self.check()
