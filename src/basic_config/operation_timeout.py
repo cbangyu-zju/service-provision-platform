2882dc36a2bd575f5da4b69abb5e0704e55e0a1d
caibangyu <cbangyu@gmail.com>
2018-12-07 18:22:29 +0800

    fix OperationTimeout

--- src/basic_config/operation_timeout.py
+++ src/basic_config/operation_timeout.py
@@ -1,10 +1,10 @@
 from . import base
 
 
-class OperationTime(base.Base):
+class OperationTimeout(base.Base):
 
     def __init__(self, system, version):
-        super(OperationTime, self).__init__(system, version)
+        super(OperationTimeout, self).__init__(system, version)
         self._op_file = "/etc/pam.d/system-auth"
         self._status = self.check()
 
